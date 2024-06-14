8a1bb2e5dffe56525ff2df1bac9a50ee6462d142
832022 <hotsuma_kawashima@densan-s.co.jp>
2024-06-14 10:28:40 +0900

    fix: file count is wrong when there is no file to rename

--- remove-time-stamp.sh
+++ remove-time-stamp.sh
@@ -11,9 +11,11 @@ fi
 
 ESC=$(printf '\033')
 
-echo $files | xargs -n1 | grep -E '_[0-9]{4}[0-9]{2}[0-9]{2}[0-9]{6}\..+' | sed -E "s/(.+)(_[0-9]{4}[0-9]{2}[0-9]{2}[0-9]{6})(\..+)/mv \1${ESC}[31m\2${ESC}[m\3 -> \1\3/"
+files=$(echo $files | xargs -n1 | grep -E '_[0-9]{4}[0-9]{2}[0-9]{2}[0-9]{6}\..+')
 
-count=$(echo $files | xargs -n1 | grep -E '_[0-9]{4}[0-9]{2}[0-9]{2}[0-9]{6}\..+' | wc -l)
+echo $files | xargs -n1 | sed -E "s/(.+)(_[0-9]{4}[0-9]{2}[0-9]{2}[0-9]{6})(\..+)/mv \1${ESC}[31m\2${ESC}[m\3 -> \1\3/"
+
+count=$(echo $files | xargs -n1 | sed '/^$/d' | wc -l)
 
 if [ $count -eq 0 ]; then
   echo "No files to rename."
@@ -25,7 +27,7 @@ read -p "$count will be renamed. Do you want to continue? [y/n] " answer
 
 case $answer in
   [Yy]* )
-    echo $files | xargs -n1 | grep -E '_[0-9]{4}[0-9]{2}[0-9]{2}[0-9]{6}\..+' | sed -E "s/(.+)(_[0-9]{4}[0-9]{2}[0-9]{2}[0-9]{6})(\..+)/mv \1\2\3 \1\3/" | bash
+    echo $files | xargs -n1 | sed -E "s/(.+)(_[0-9]{4}[0-9]{2}[0-9]{2}[0-9]{6})(\..+)/mv \1\2\3 \1\3/" | bash
     ;;
   * )
     echo "Canceled."
